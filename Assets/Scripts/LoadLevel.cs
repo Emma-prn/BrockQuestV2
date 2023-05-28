466aab27671c9edf7cabb1d1027db9e5fb507a18
Emma <emma.pernin1@gmail.com>
2023-05-28 18:44:28 +0200

    [Transition] Add transition when scene change

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 public class LoadLevel : MonoBehaviour
 {
     public string area_to_load;
+    public Transition transition;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
-            SceneManager.LoadScene(area_to_load);
+            //SceneManager.LoadScene(area_to_load);
+            transition.LoadLevel(area_to_load);
         }
     }
 }
